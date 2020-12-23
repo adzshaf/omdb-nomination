31d5f1a3f646ec247faca517cf690cb9761fa012
Shafiya Adzhani <adz.arsym@gmail.com>
2020-12-24 04:30:43 +0800

    add navbar and its styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <h1>The Shoppies</h1>
+    </div>
+  );
+};
+
+export default Navbar;
