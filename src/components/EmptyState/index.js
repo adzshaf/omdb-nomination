869081282646ebb0c5d76641926f66f6b31088c4
Shafiya Adzhani <adz.arsym@gmail.com>
2021-01-05 23:35:29 +0800

    add empty state

---
+++ src/components/EmptyState/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Search from "../../image/search.png";
+import "./style.css";
+
+const EmptyState = () => {
+  return (
+    <div className="empty-state-container">
+      <img src={Search} className="search-image" />
+      <div>
+        <h2>You haven't nominated yet!</h2>
+        <p>Add your favorite movies to be nominated</p>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyState;
