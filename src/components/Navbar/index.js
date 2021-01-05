63f1b5482aa853d80f24619913b66fe1c7a8aaa9
Shafiya Adzhani <adz.arsym@gmail.com>
2021-01-06 02:39:08 +0800

    add result item for search result

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,11 +4,20 @@ import apiService from "../../api/index";
 
 const Navbar = () => {
   const [search, setSearch] = useState("");
+  const [result, setResult] = useState("");
+
+  console.log(result);
 
   useEffect(() => {
     apiService
       .getSearch(search)
-      .then((res) => console.log(res))
+      .then((res) => {
+        if (res.data.Response === "False") {
+          setResult(res.data.Error);
+        } else {
+          setResult(res.data.Search);
+        }
+      })
       .catch((err) => console.log(err));
   }, [search]);
 
@@ -16,41 +25,57 @@ const Navbar = () => {
     <div className="navbar-container">
       <h1>The Shoppies</h1>
       <div className="input-container">
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M8 12C5.791 12 4 10.209 4 8C4 5.791 5.791 4 8 4C10.209 4 12 5.791 12 8C12 10.209 10.209 12 8 12ZM17.707 16.293L12.887 11.473C13.585 10.492 14 9.296 14 8C14 4.687 11.313 2 8 2C4.687 2 2 4.687 2 8C2 11.313 4.687 14 8 14C9.296 14 10.492 13.585 11.473 12.887L16.293 17.707C16.488 17.902 16.744 18 17 18C17.256 18 17.512 17.902 17.707 17.707C18.098 17.316 18.098 16.684 17.707 16.293Z"
-            fill="#212B36"
-          />
-          <mask
-            id="mask0"
-            mask-type="alpha"
-            maskUnits="userSpaceOnUse"
-            x="2"
-            y="2"
-            width="17"
-            height="16"
+        <div className="icon-input">
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
             <path
               fill-rule="evenodd"
               clip-rule="evenodd"
               d="M8 12C5.791 12 4 10.209 4 8C4 5.791 5.791 4 8 4C10.209 4 12 5.791 12 8C12 10.209 10.209 12 8 12ZM17.707 16.293L12.887 11.473C13.585 10.492 14 9.296 14 8C14 4.687 11.313 2 8 2C4.687 2 2 4.687 2 8C2 11.313 4.687 14 8 14C9.296 14 10.492 13.585 11.473 12.887L16.293 17.707C16.488 17.902 16.744 18 17 18C17.256 18 17.512 17.902 17.707 17.707C18.098 17.316 18.098 16.684 17.707 16.293Z"
-              fill="white"
+              fill="#212B36"
             />
-          </mask>
-          <g mask="url(#mask0)"></g>
-        </svg>
-        <input
-          placeholder="Search movie title"
-          onChange={(e) => setSearch(e.target.value)}
-        ></input>
+            <mask
+              id="mask0"
+              mask-type="alpha"
+              maskUnits="userSpaceOnUse"
+              x="2"
+              y="2"
+              width="17"
+              height="16"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8 12C5.791 12 4 10.209 4 8C4 5.791 5.791 4 8 4C10.209 4 12 5.791 12 8C12 10.209 10.209 12 8 12ZM17.707 16.293L12.887 11.473C13.585 10.492 14 9.296 14 8C14 4.687 11.313 2 8 2C4.687 2 2 4.687 2 8C2 11.313 4.687 14 8 14C9.296 14 10.492 13.585 11.473 12.887L16.293 17.707C16.488 17.902 16.744 18 17 18C17.256 18 17.512 17.902 17.707 17.707C18.098 17.316 18.098 16.684 17.707 16.293Z"
+                fill="white"
+              />
+            </mask>
+            <g mask="url(#mask0)"></g>
+          </svg>
+          <div className="search-input">
+            <input
+              placeholder="Search movie title"
+              onChange={(e) => setSearch(e.target.value)}
+            ></input>
+          </div>
+        </div>
+        <div className="search-result">
+          {Array.isArray(result) ? (
+            result.map((value, index) => (
+              <div key={index} className="search-item">
+                <p>{value.Title}</p>
+                <p>({value.Year})</p>
+              </div>
+            ))
+          ) : (
+            <p>{result}</p>
+          )}
+        </div>
       </div>
     </div>
   );
