fd146e4e3ea19c3729ff5d4fb959c45a1ec6f81c
Shafiya Adzhani <adz.arsym@gmail.com>
2021-01-06 06:32:04 +0800

    add banner if user already nominated for 5 movies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import logo from "./image/search.png";
-import { Counter } from "./features/counter/Counter";
 import NominationSection from "./components/NominationSection";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Banner from "./components/Banner";
+import { useSelector } from "react-redux";
+import { selectNomination } from "./features/nomination/nominationSlice";
 
 function App() {
+  const nomination = useSelector(selectNomination);
+
   return (
     <div className="App">
+      {nomination.length === 5 && <Banner />}
       <Navbar />
       <NominationSection />
     </div>
---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Banner = () => {
+  return (
+    <div className="banner-container">
+      <h2>You have nominated 5 movies</h2>
+    </div>
+  );
+};
+
+export default Banner;
