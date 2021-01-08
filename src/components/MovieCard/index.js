1ed1214a22b419cc3ad4c90414012919a6b3aa3b
Shafiya Adzhani <adz.arsym@gmail.com>
2021-01-09 02:24:54 +0800

    handle if movie image is not available

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -4,7 +4,11 @@ import "./style.css";
 const MovieCard = ({ imageSource, title, year, onClick, isShared }) => {
   return (
     <div className="movie-card-container">
-      <img className="movie-card-image" src={imageSource} alt={title} />
+      {imageSource !== "N/A" ? (
+        <img className="movie-card-image" src={imageSource} alt={title} />
+      ) : (
+        <div className="movie-card-not-available">Image not available</div>
+      )}
       <div className="movie-card-detail">
         <h2>{title}</h2>
         <p>({year})</p>
