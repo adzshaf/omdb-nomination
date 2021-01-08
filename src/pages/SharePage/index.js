fd9bed6b75f42822690e1abfd07905616e29ce35
Shafiya Adzhani <adz.arsym@gmail.com>
2021-01-09 00:07:42 +0800

    add button nominate on share page and fix mobile styling

--- src/pages/SharePage/index.js
+++ src/pages/SharePage/index.js
@@ -1,13 +1,19 @@
-import { useParams } from "@reach/router";
+import { useParams, Link } from "@reach/router";
 import NominationSection from "../../components/NominationSection";
+import Button from "../../components/Button";
+import "./style.css";
 
 const Share = () => {
   const params = useParams();
   const idArray = params.shareId.split("-");
   return (
-    <div>
+    <div className="share-container">
       <h2>Your Friend's Nomination List</h2>
       <NominationSection isShared data={idArray} />
+      <h2>Nominate your favorite movies!</h2>
+      <Link to="/">
+        <Button>Nominate</Button>
+      </Link>
     </div>
   );
 };
